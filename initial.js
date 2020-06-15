ade444b10972d1d97a6b11d8c1ea5e02dfaaccbc
Iurii <doroshiurii@gmail.com>
2020-06-15 18:16:55 +0300

    Fist commit

--- initial.js
+++ initial.js
@@ -1,1 +1,14 @@
-#test
+let numberOfFilms = +prompt("Скільки ви фільмів вже подивилися?", 0),
+    lastMovie = prompt(`Один з останніх переглянутих фільмів?`),
+    movieScore = prompt(`На скільки його оцінете?`);
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    private: false
+}
+
+personalMovieDB.movies[lastMovie] = movieScore;
